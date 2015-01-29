11f32cbaf53d773179bc32b05da0078f5dbff3f7
Brian Sipple <Bsipple57@gmail.com>
2015-01-29 12:56:26 -0600

    Header for fileToBuffer Utility and .gitignore

---
+++ Util.h
@@ -0,0 +1,8 @@
+// Namespace for our utility function that will read a file and
+// return the contents of that file as a C string.
+#include <stdlib.h>
+#include <stdio.h>
+
+namespace Util {
+    char* fileToBuffer(char *file);
+}
