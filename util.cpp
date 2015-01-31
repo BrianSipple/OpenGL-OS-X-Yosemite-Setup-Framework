724c18606452b2011c46c208596053f67f887609
Brian Sipple <Bsipple57@gmail.com>
2015-01-31 08:22:34 -0600

    fileToBuffer Util for loading shader data

---
+++ util.cpp
@@ -0,0 +1,26 @@
+// utility function that will read a file and
+// return the contents of that file as a C string.
+#include "Util.h"
+
+namespace Util {
+    char* fileToBuffer(char *file) {
+
+        FILE *fptr;
+        long length;
+        char *buf;
+
+        fptr = fopen(file, "rb"); /* Open file for reading */
+        if (!fptr) { /* Return NULL on failure */
+            return NULL;
+        }
+        fseek(fptr, 0, SEEK_END); /* Seek to the end of the file */
+        length = ftell(fptr); /* Find out how many bytes into the file we are */
+        buf = (char*)malloc(length+1); /* Allocate a buffer for the entire length of the file and a null terminator */
+        fseek(fptr, 0, SEEK_SET); /* Go back to the beginning of the file */
+        fread(buf, length, 1, fptr); /* Read the contents of the file in to the buffer */
+        fclose(fptr); /* Close the file */
+        buf[length] = 0; /* Null terminator */
+
+        return buf; /* Return the buffer */
+    }
+}
